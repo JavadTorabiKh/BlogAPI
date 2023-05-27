cd5fc94e42084290465213376f4c71a0f635e85e
javadTorabi <j.2528840@gmail.com>
2023-05-27 09:56:44 +0330

    document

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # 3rd-party-app
+    "django.contrib.sites",
+    # 3rd-party-app ------------------------
     "rest_framework",
     "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     "rest_auth",
-    # my app local
+    "rest_auth.registration",
+    # my app local---------------------------
     "posts",
 ]
 
@@ -137,4 +142,10 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
+
+SITE_ID = 1
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("posts.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/rest-auth/", include("rest_auth.urls")),
+    path("api/v1/rest-auth/registration/", include("rest_auth.registration.urls")),
+    path("schema/", get_schema_view(title="Blog API", version="1.0.0")),
+    path("docs/", include_docs_urls(title="Blog API")),
+    
 ]
